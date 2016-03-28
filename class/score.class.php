9cb42447ac7f1512811593097d8dcba7943f856e
Amanskywalker <ak47su30ac@gmail.com>
2016-03-28 23:14:30 +0530

    deleted:    README~ 	modified:   class/score.class.php 	modified:   class/time.class.php

--- class/score.class.php
+++ class/score.class.php
@@ -6,14 +6,14 @@
 
 		it shows the score of the user
 	*/
-	class score extends user 		
+	class score extends time   		
 	{
 		
 		public $sid;			// primary index score id
 		public $iid;			// userid or teamid
-		public $score 			// user or team score
-		private $time_updated 	// time last answer updated
-		public $plenty			// number of wrong answer submitted
+		public $score; 			// user or team score
+		private $time_updated; 	// time last answer updated
+		public $plenty;			// number of wrong answer submitted
 		/*
 			no need of constructor
 		function __construct(argument)
@@ -21,11 +21,6 @@
 			# code...
 		}
 		*/
-
-		function get_rem_time($tid) 	// get the remaning time of the given team
-		{
-			
-		}
 
 		function get_score($id)			// get the score of a given $iid as $id
 		{
@@ -36,9 +31,63 @@
 			$result=$connection->query("SELECT * FROM `score` WHERE `iid`='$id'");
             $rows=$result->fetch_array();
 
+            $this->sid=$rows[0];
+            $this->iid=$rows[1];
+            $this->score=$rows[2];
+            $this->time_updated=$rows[3];
+            $this->plenty=$rows[4];
+
             //close the database connection 
             mysqli_close($connection);
 		}
 
+		function wrong_submition($uid)
+		{
+			get_score($uid);
+
+			$time=date('H:i:s');			// to get surrent system time
+			$date=date_create($time);		// to convert it into date formate
+
+			$this->time_updated=date_format($date,"H:i:s");
+			
+			$this->plenty+=-1;
+
+			include 'dbms/dbms_imp.php';
+
+			$update_query="UPDATE `score` SET `ltime`='$this->time_updated',`plenty`='$this->plenty' WHERE `sid` = '$this->sid'";
+			
+			$mysql_query_run=$connection->query($update_query);
+
+			//close the database connection 
+            mysqli_close($connection);
+		}
+
+		function correct_submition($uid)
+		{
+			get_score($uid);
+
+			$time=date('H:i:s');			// to get surrent system time
+			$date=date_create($time);		// to convert it into date formate
+
+			$this->time_updated=date_format($date,"H:i:s");
+			
+			$this->score+=10;
+
+			include 'dbms/dbms_imp.php';
+
+			$update_query="UPDATE `score` SET `score`='$this->score',`ltime`='$this->time_updated' WHERE `sid` = '$this->sid'";
+			
+			$mysql_query_run=$connection->query($update_query);
+
+			//close the database connection 
+            mysqli_close($connection);
+		}
+
+		function add_entry($uid)
+		{
+			$this->iid=$uid;
+
+
+		}
 	}
 ?>
--- class/time.class.php
+++ class/time.class.php
@@ -4,12 +4,51 @@
 	/*
 		keeps track of time for each team or user
 	*/
-	class ClassName extends AnotherClass
+	class time
 	{
+		private $tid;		// unique id of table
+		private $uid;		// user or team id
+		private $s_time		// start time
+		private $e_time 	// end time
 		
-		function __construct(argument)
+		function add_entry($uid,$add_time)	// function to add the entry to the database
 		{
-			# code...
+			$this->uid=$uid;
+			
+			$time=date('H:i:s');			// to get surrent system time
+			$date=date_create($time);		// to convert it into date formate
+
+			$this->s_time=date_format($date,"H:i:s");	// to add as start time
+			
+			date_add($date,date_interval_create_from_date_string("$add_time hours"));		// to add given time in it
+
+			$this->e_time=date_format($date,"H:i:s");	// to add end time
+
+			include 'dbms/dbms_imp.php';
+
+			$insert_query="INSERT INTO `etime` (`tid`, `uid`, `start`, `end`) 
+				VALUES ('','$this->uid',$this->s_time','$this->e_time')";
+			
+			$mysql_query_run=$connection->query($insert_query);
+			
+			mysqli_close($connection);
 		}
+
+		function get_end_time($uid) 	// function to get end time for the mentioned user 
+		{
+			$this->uid=$uid;
+			
+			include 'dbms/dbms_imp.php';
+
+			$result=$connection->query("SELECT `end` FROM `etime` WHERE `uid`='$uid'");
+            $rows=$result->fetch_array();
+			
+			$this->e_time=$rows[0];
+
+			mysqli_close($connection);
+
+			return $this->e_time;
+		}
+
 	}
 ?>
