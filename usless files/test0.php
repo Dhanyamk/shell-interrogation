b3da570bb128e53a5cc9ecae1bf8c8f20602426a
Amanskywalker <ak47su30ac@gmail.com>
2016-09-13 21:52:24 +0530

    modified:   .gitignore  	deleted:    usless files/addqu.html  	deleted:    usless files/admlo.html  	deleted:    usless files/bst.php  	deleted:    usless files/digital-numbers/0.png  	deleted:    usless files/digital-numbers/1.png  	deleted:    usless files/digital-numbers/2.png  	deleted:    usless files/digital-numbers/3.png  	deleted:    usless files/digital-numbers/4.png  	deleted:    usless files/digital-numbers/5.png  	deleted:    usless files/digital-numbers/6.png  	deleted:    usless files/digital-numbers/7.png 	deleted:    usless files/digital-numbers/8.png  	deleted:    usless files/digital-numbers/9.png  	deleted:    usless files/digital-numbers/bkgd.gif  	deleted:    usless files/digital-numbers/colon.png  	deleted:    usless files/err.html 	deleted:    usless files/game.html  	deleted:    usless files/jquery.timers-1.1.2.js  	deleted:    usless files/lp2.php 	deleted:    usless files/lp3.php  	deleted:    usless files/ques1.html 	deleted:    usless files/refresh.current.php 	deleted:    usless files/score1.html  	deleted:    usless files/test.php  	deleted:    usless files/test0.php  	deleted:    usless files/timer.html  	deleted:    usless files/timer.php  	deleted:    usless files/timer1.php  	deleted:    usless files/timer3.php  	deleted:    usless files/timer4.php

--- usless files/test0.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ip=$_SERVER['REMOTE_ADDR'];
-$host=$_SERVER['REMOTE_HOST'];
-$user=$_SERVER['REMOTE_USER'];
-$useragent=$_SERVER['HTTP_USER_AGENT'];
-$port=$_SERVER['REMOTE_PORT'];
-$url=$ip.':'.$port;
-echo "$url <br/> $host <br/> $user <br/> $useragent";
-?>
