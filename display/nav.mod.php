9bf0be4d4a23ae4decb7c7adf2bb34a42d891add
Amanskywalker <ak47su30ac@gmail.com>
2016-09-13 21:49:41 +0530

    modified:   display/nav.mod.php

--- display/nav.mod.php
+++ display/nav.mod.php
@@ -31,12 +31,11 @@
                 <?php
               }
             ?>
-            <!--<li><a href="#">Questions</a></li>-->
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Questions <span class="caret"></span></a>
               <ul class="dropdown-menu">
                 <?php
-                  $scored= new score;
+                  $scored = new score;
                   $scored->get_score($teamid);
                   $n=($scored->score)/10;
                   for ($i=0; $i <=$n ; $i++)
@@ -47,19 +46,12 @@
                   <?php
                   }
                 ?>
-                <!--<li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>-->
               </ul>
             </li>
             <li><a href="score.php">Leaderboards</a></li>
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
-            <!--<li id="time"><script>disptime();</script></li>-->
             <?php
               if ($debug && false)
               {
